b1f25578bd7de1fd08e87470e4e9ebf2c251076a
PrathameshWalunj <prathameshwalunj27@gmail.com>
2024-12-22 04:51:46 -0500

    one FINAL time

--- pkg/nvml/wrapper.go
+++ pkg/nvml/wrapper.go
@@ -2,7 +2,7 @@ package nvml
 
 /*
 #cgo CFLAGS: -I"C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.6/include"
-#cgo windows LDFLAGS: -lnvml
+#cgo windows LDFLAGS: -L"C:/Windows/System32" -L"${SRCDIR}/../../libs" -lnvml
 #cgo linux LDFLAGS: -lnvidia-ml
 #include <nvml.h>
 */
